ffd8225acf663d6ace734823281b77952f56fe14
Sabrina Belloni <sabrina82belloni@gmail.com>
2024-12-17 19:33:14 +0000

    create the api call for retrieve data

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -14,4 +14,26 @@ function fetchGitHubInformation(event) {
         `<div id="loader">
         <img src="assets/css/loader.gif" alt="loading..." />
         </div>`);
+    //4. create a promises for retrieve data from github 
+    //promises two things correlated when one thing is done then do the other
+    $.when(
+        //when get a response from the api then run a function below
+        //when get a function as argument getJSON() with the github api address
+        $.getJSON(`https://api.github.com/users/${username}`)
+    ).then(
+        //display the information in the div gh-user-data using the fucntion response()
+        function(response) {
+            var userData = response;
+            $("gh-user-data").html(userinformationHTML(userData));
+        }, function(errorResponse) {
+            //add function that handle the errors 
+            if (errorResponse.status === 404) {
+                $("gh-user-data").html(
+                    `<h2>no info found for user ${username}</h2>`);
+            } else {
+                console.log(errorResponse);
+                $("#gh-user-data").html(
+                    `<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
+            }
+        });
 }
