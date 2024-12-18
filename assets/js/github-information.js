9a0ca8564395529de7c78f969d2590dd594776b6
Sabrina Belloni <sabrina82belloni@gmail.com>
2024-12-18 19:20:01 +0000

    add the username search function

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -2,22 +2,23 @@
 //function return the object returned from the github api
 //returned in a formatted form
 function userInformationHTML(user) {
-    // return the user public display name and the public profile
+    //user is the object returned from the APi and it contains many methods
+    // return the user public display name and the public profile in formatted form
     return `
         <h2>${user.name}
             <span class="small-name">
-                (@<a href="${user.html_url}" target="_blank>${user.login}</a>
+                (@<a href="${user.html_url}" target="_blank">${user.login}</a>)
             </span>
         </h2>
         <div class="gh-content">
             <div class="gh-avatar">
                 <a href="${user.html_url} target="_blank">
-                    <img src="${user.avatar_url}" width="80" alt="${user.login}" />
+                    <img src="${user.avatar_url}" width="80" height="80" alt="${user.login}" />
                 </a>
             </div>
-            <p> followers: ${user.followers} - following ${user.following} <br> Repos: ${user.public_repos}</p>
+            <p> followers: ${user.followers} - following: ${user.following} <br> Repos: ${user.public_repos}</p>
         </div>
-        `
+        `;
 }
 
 
@@ -42,16 +43,20 @@ function fetchGitHubInformation(event) {
     $.when(
         //when get a response from the api then run a function below
         //when get a function as argument getJSON() with the github api address
-        $.getJSON(`https://api.github.com/users/${username}`)
+        $.getJSON(`https://api.github.com/users/${username}`),
+        $.getJSON(`https://api.github.com/users/${username}/repos`)
     ).then(
         //display the information in the div gh-user-data using the fucntion response()
-        function(response) {
-            var userData = response;
-            $("gh-user-data").html(userInformationHTML(userData));
+        function(firstResponse, secondResponse) {
+            var userData = firstResponse[0];
+            var repoData = secondResponse[0];
+            $("#gh-user-data").html(userInformationHTML(userData));
+            $("#gh-repo-data").html(repoInformationHTML(repoData))
+
         }, function(errorResponse) {
             //add function that handle the errors 
             if (errorResponse.status === 404) {
-                $("gh-user-data").html(
+                $("#gh-user-data").html(
                     `<h2>no info found for user ${username}</h2>`);
             } else {
                 console.log(errorResponse);
