2e6598e2f2b90d0ae7e127e06c3447fa724c5998
Sabrina Belloni <sabrina82belloni@gmail.com>
2024-12-19 16:05:52 +0000

    add the js file for mail address sent

--- assets/js/sendEmail.js
+++ assets/js/sendEmail.js
@@ -1,1 +1,15 @@
-console.log("hello")
+function sendMail(contactForm) {
+    emailjs.send("service_dw6g1yc","template_nghxt3v", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "project_request": contactForm.projectsummary.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log( "FAILED", error);
+        });
+    return false;
+}
